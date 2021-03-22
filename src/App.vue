0326ed92b6dfc93c152f0b2cb91d76e98857a2e7
ka-tanaka-razoredge <software@razor-edge.net>
2021-03-22 09:39:24 +0900

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="https://vuejs.org/images/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <ListView></ListView>
   </div>
 </template>
 
 <script>
-import ListView from "./components/ListView.vue";
+  import ListView from "./components/ListView.vue";
 
 export default {
   name: "App",
@@ -17,12 +16,12 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
