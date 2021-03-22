11bb4badc0134c86a9188d1d9dd8ec6a32228511
ka-tanaka-razoredge <software@razor-edge.net>
2021-03-22 12:47:26 +0900

    2 files modified

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <ListView></ListView>
+    <ListView ref="list_view" @arrive="doIt"></ListView>
   </div>
 </template>
 
@@ -9,8 +9,31 @@
 
 export default {
   name: "App",
+  timer: {
+  },
+  tc: 0,
+  props: {
+  },
+  methods: {
+    doIt: function() {
+      console.log('App.methods.doIt');
+    }
+  },
   components: {
     ListView
+  },
+  created: function() {
+    const doRoutine = (function (e) {
+      if (this.tc == 3) {
+//        this.$emit('arrive');
+//        console.log('here');
+        this.$refs.list_view.dispatchEvent(new CustomEvent('arrive', null));
+      }
+      this.tc++;
+    }).bind(this);
+    this.tc = 0;
+    this.timer = setInterval(doRoutine, 1000);
+    console.log(this);
   }
 };
 </script>
--- src/components/ListView.vue
+++ src/components/ListView.vue
@@ -1,14 +1,27 @@
 <template>
-  <div style="border: 1px solid black;">
+  <div ref="lv" style="border: 1px solid black;" @arrive="handle">
     ListView
   </div>
 </template>
 
 <script>
-export default {
+  export default {
   name: "ListView",
   props: {
     msg: String
+  },
+  methods: {
+    dispatchEvent: function(e) {
+      console.log(this);
+      this.$refs.lv.dispatchEvent(new CustomEvent('arrive', null));
+    },
+    doIt: function(e) {
+    },
+    handle: function(e) {
+      console.log('handle');
+    }
+  },
+  mounted: function() {
   }
 };
 </script>
