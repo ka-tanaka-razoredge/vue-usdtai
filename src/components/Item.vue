2279b56ce06e5fe99e78b3f8c71440733ea836b7
ka-tanaka-razoredge <software@razor-edge.net>
2021-03-23 15:42:35 +0900

    1 file modified, 1 added

---
+++ src/components/Item.vue
@@ -0,0 +1,30 @@
+<template>
+  <div ref="item" style="border: 1px solid red;">
+    {{id}}
+  </div>
+</template>
+
+<script>
+  import {ref, defineComponent} from 'vue';
+
+  export default defineComponent({
+    name: "Item",
+    props: {
+      id: ""
+    },
+    methods: {
+      getDiv: function() {
+        return this.$refs.item.$el;
+      }
+    },
+    mounted: (e) => {
+      console.log(this.$refs.item);
+    },
+    getDiv: function() {
+      console.log(this);
+      console.log(this.$refs);
+      return this.$refs;
+    }
+    
+  });
+</script>
--- src/components/ListView.vue
+++ src/components/ListView.vue
@@ -1,27 +1,41 @@
 <template>
-  <div ref="lv" style="border: 1px solid black;" @arrive="handle">
+  <div id="lv" ref="lv" style="border: 1px solid black;" @arrive="handle">
     ListView
   </div>
 </template>
 
 <script>
+  import {createApp, defineComponent} from 'vue';
+  import Item from './Item.vue';
+
   export default {
-  name: "ListView",
-  props: {
-    msg: String
-  },
-  methods: {
-    dispatchEvent: function(e) {
-      console.log(this);
-      this.$refs.lv.dispatchEvent(e);
+    name: "ListView",
+    components: {
+      Item
     },
-    doIt: function(e) {
+    props: {
+      msg: String
     },
-    handle: function(e) {
-      console.log('handle');
+    methods: {
+      dispatchEvent: function(e) {
+        console.log(this);
+        this.$refs.lv.dispatchEvent(e);
+      },
+      doIt: function(e) {
+      },
+      handle: function(e) {
+        let vueInstance = createApp({});
+        vueInstance.component('Item', {
+          template: '<div>item</div>',
+        });
+        let div = document.createElement('div');
+        div.id = "item";
+        div.innerHTML = 'innerHTML';
+//        this.$refs.lv.appendChild(vueInstance.$el);
+        vueInstance.mount(this);
+      }
+    },
+    mounted: function() {
     }
-  },
-  mounted: function() {
-  }
 };
 </script>
